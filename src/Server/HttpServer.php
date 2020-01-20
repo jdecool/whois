b03331998c14b1d60db51a9f3be5546ddc4041e9
Jérémy DECOOL <contact@jdecool.fr>
2020-01-20 20:48:51 +0100

    Implement FastRouter

--- src/Server/HttpServer.php
+++ src/Server/HttpServer.php
@@ -4,8 +4,13 @@ declare(strict_types=1);
 
 namespace JDecool\Whois\Server;
 
+use FastRoute\{
+    Dispatcher,
+    RouteCollector,
+};
 use JDecool\Whois\WhoisClient;
 use JsonException;
+use LogicException;
 use Psr\Http\{
     Message\ResponseInterface,
     Message\ServerRequestInterface,
@@ -19,6 +24,7 @@ use React\Http\{
     Server,
 };
 use React\Socket\Server as Socket;
+use function FastRoute\simpleDispatcher;
 
 class HttpServer
 {
@@ -31,32 +37,37 @@ class HttpServer
         $this->client = $client;
         $this->loop = Factory::create();
         $this->socket = new Socket(sprintf('%s:%d', $ip, $port), $this->loop);
+
+        $this->dispatcher = simpleDispatcher(function(RouteCollector $routes) {
+            $routes->addRoute('POST', '/whois', function(ServerRequestInterface $request): ResponseInterface {
+                try {
+                    $data = json_decode($request->getBody()->getContents(), true, JSON_THROW_ON_ERROR);
+                } catch (JsonException $e) {
+                    return new Response(400, ['Content-Type' => 'text/plain'], "Invalid request ({$e->getMessage()})");
+                }
+
+                return new Response(200, ['Content-Type' => 'text/plain'], $this->client->whois($data['domain']));
+            });
+        });
     }
 
     public function run(): void
     {
-        $server = new Server([$this, 'handleRequest']);
+        $server = new Server(function(ServerRequestInterface $request) {
+            $route = $this->dispatcher->dispatch($request->getMethod(), $request->getUri()->getPath());
+
+            switch ($route[0]) {
+                case Dispatcher::NOT_FOUND:
+                    return new Response(404, ['Content-Type' => 'text/plain'],  'Not found');
+                case Dispatcher::FOUND:
+                    $params = $route[2];
+                    return $route[1]($request, ... array_values($params));
+            }
+
+            throw new LogicException('Something wrong with routing');
+        });
         $server->listen($this->socket);
 
         $this->loop->run();
-    }
-
-    public function handleRequest(ServerRequestInterface $request): ResponseInterface
-    {
-        if ('/whois' !== $request->getUri()->getPath()) {
-            return new Response(404);
-        }
-
-        if ('POST' !== $request->getMethod()) {
-            return new Response(400, ['Content-Type' => 'text/plain'], 'Invalid HTTP method');
-        }
-
-        try {
-            $data = json_decode($request->getBody()->getContents(), true, JSON_THROW_ON_ERROR);
-        } catch (JsonException $e) {
-            return new Response(400, ['Content-Type' => 'text/plain'], "Invalid request ({$e->getMessage()})");
-        }
-
-        return new Response(200, ['Content-Type' => 'text/plain'], $this->client->whois($data['domain']));
     }
 }
