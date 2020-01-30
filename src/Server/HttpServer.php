48b391e9768dd65a880889cbfac20f43f409145f
Jérémy DECOOL <contact@jdecool.fr>
2020-01-30 07:31:02 +0100

    Add DNS api

--- src/Server/HttpServer.php
+++ src/Server/HttpServer.php
@@ -8,7 +8,10 @@ use FastRoute\{
     Dispatcher,
     RouteCollector,
 };
-use JDecool\Whois\WhoisClient;
+use JDecool\Whois\{
+    DnsClient,
+    WhoisClient,
+};
 use JsonException;
 use LogicException;
 use Psr\Http\{
@@ -32,16 +35,39 @@ class HttpServer
     private LoopInterface $loop;
     private Socket $socket;
     private WhoisClient $client;
+    private DnsClient $dns;
 
-    public function __construct(WhoisClient $client, string $ip = '0.0.0.0', int $port = 8000)
+    public function __construct(WhoisClient $client, DnsClient $dns, string $ip = '0.0.0.0', int $port = 8000)
     {
         $this->client = $client;
+        $this->dns = $dns;
         $this->loop = Factory::create();
         $this->socket = new Socket(sprintf('%s:%d', $ip, $port), $this->loop);
 
         $this->dispatcher = simpleDispatcher(function(RouteCollector $routes) {
             $routes->addRoute('GET', '/', static function(ServerRequestInterface $request): ResponseInterface {
                 return new Response(200, ['Content-Type' => 'text/plain'], "Service is running");
+            });
+
+            $routes->addRoute('POST', '/dns', function(ServerRequestInterface $request): ResponseInterface {
+                try {
+                    $data = json_decode($request->getBody()->getContents(), true, JSON_THROW_ON_ERROR);
+                } catch (JsonException $e) {
+                    return new Response(400, ['Content-Type' => 'text/plain'], "Invalid request ({$e->getMessage()})");
+                } catch (Throwable $e) {
+                    return new Response(500, ['Content-Type' => 'text/plain'], "Internal Server Error ({$e->getMessage()})");
+                }
+
+                if (!isset($data['domain'])) {
+                    return new Response(400, ['Content-Type' => 'text/plain'], "Missing domain key");
+                }
+
+                $domain = parse_url($data['domain'], PHP_URL_HOST);
+                if (!is_string($domain)) {
+                    return new Response(400, ['Content-Type' => 'text/plain'], "Invalid domain");
+                }
+
+                return new Response(200, ['Content-Type' => 'text/plain'], $this->dns->resolve($domain));
             });
 
             $routes->addRoute('GET', '/ip', static function(ServerRequestInterface $request): ResponseInterface {
--- src/Server/bootstrap.php
+++ src/Server/bootstrap.php
@@ -5,14 +5,15 @@ declare(strict_types=1);
 namespace JDecool\Whois\Server;
 
 use JDecool\Whois\{
+    DnsClient,
     SocketFactory,
-    WhoisClient,
-};
+    WhoisClient};
 
 function bootstrap(string $configurationFile, string $ip, int $port): HttpServer
 {
     return new HttpServer(
         WhoisClient::fromConfiguration($configurationFile, new SocketFactory()),
+        new DnsClient(),
         $ip,
         $port,
     );
